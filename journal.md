915735278fac0e8ce57200421c8e862f1fd2bd92
Zelie-C <zelie.chemin@gmail.com>
2023-09-21 12:06:24 +0200

    nouveaux attributs character

--- journal.md
+++ journal.md
@@ -1,9 +1,9 @@
 # 21/09/2023
 
-1. Refactoring code :
+1. Refactoring code (fin 11h15):
     - changement attribut public en privé et renommage
     - ajouts accesseurs pour CHAQUE attribut
     - réécriture code pour qu'il fonctionne avec le nommage et les accesseurs.
 
-2. Changement class Viking en perso de base sans type
+2. Changement class Viking en perso de base sans type (fin 11h30)
 3. Ajouts des class classes de personnages par héritage (extends)
--- src/Character.ts
+++ src/Character.ts
@@ -2,19 +2,27 @@ import { Arme } from "./Arme";
 
 export class Character {
     private _nom: string;
+    private _level: number;
+    private _exp: number;
+    private _characterType: CharacterType;
     private _pv: number;
-    private _pvMax: number;
     private _force: number;
-    private _defense: number;
-    private _arme: Arme;
+    private _speed: number;
+    private _intelligence: number;
+    private _pm: number;
+    private _critic: number;
 
-    constructor(nom: string, pv: number, force: number, defense: number, arme: Arme){
+    constructor(nom: string){
         this._nom = nom;
-        this._pv = pv;
-        this._pvMax = pv
-        this._force = force;
-        this._defense = defense
-        this._arme = arme;
+        this._level = 1;
+        this._exp = 0;
+        this._pv = 50;
+        this._characterType = null;
+        this._force = 10;
+        this._speed = 10;
+        this._intelligence = 10
+        this._pm = 50;
+        this._critic = 0.02;
     }
 
     get nom(): string{
@@ -25,20 +33,36 @@ export class Character {
       this._nom = newName;
     }
 
+    get level(): number{
+      return this._level;
+    }
+
+    set level(newLevel: number) {
+      this._level = newLevel;
+    }
+
+    get exp(): number{
+      return this._exp;
+    }
+
+    set exp(newExp: number) {
+      this._exp = newExp;
+    }
+
+    get characterType(): CharacterType{
+      return this._characterType;
+    }
+
+    set characterType(newCharacterType: CharacterType) {
+      this._characterType = newCharacterType;
+    }
+
     get pv(): number{
       return this._pv;
     }
 
     set pv(newPv: number) {
       this._pv = newPv;
-    }
-
-    get pvMax(): number {
-      return this._pvMax;
-    }
-
-    set pvMax(newPvMax: number) {
-      this._pvMax = newPvMax
     }
 
     get force(): number {
@@ -49,49 +73,65 @@ export class Character {
       this._force = newForce;
     }
 
-    get defense(): number {
-      return this._defense;
+    get speed(): number {
+      return this._speed;
     }
 
-    set defense(newDefense: number) {
-      this._defense = newDefense;
+    set speed(newSpeed: number) {
+      this._speed = newSpeed;
     }
 
-    get arme(): Arme {
-      return this._arme;
+    get intelligence(): number {
+      return this._intelligence;
     }
 
-    set arme(newArme: Arme) {
-      this._arme = newArme
+    set intelligence(newIntelligence: number) {
+      this._intelligence = newIntelligence;
     }
 
-    public attack(): number {
-      let damage: number;
-      let coupCritique = Math.random();
-      if (coupCritique < 0.1) {
-        damage = this.force *2;
-        console.log(`${this.nom} fait un coup critique et inflige ${damage} de dégats`)
-      } else {
-        damage = this.force
-        console.log(`${this.nom} inflige ${damage} de dégats`)
-      }
-      if (this.arme.possedeVolDeVie() && this.pv < this.pvMax){
-        let stealedPv = Math.trunc((this.force) * 0.13);
-        if (this.pv + stealedPv > this.pvMax) {
-          console.log(`${this.nom} récupère tous ses points de vie`)
-          this.pv += this.pvMax
-        } else {
-          this.pv += this.pv + stealedPv
-          console.log(`${this.nom} récupère ${stealedPv} points de vie`)
-        }
-      } else if (this.arme.possedeVolDeVie() && this.pv === this.pvMax) {
-        console.log(`${this.nom} ne récupère pas de vie`)
-      }
-    return damage;
-  }
+    get pm(): number {
+      return this._pm;
+    }
 
-  public takeDamage(damage: number) {
-    console.log(`${this.nom} perd ${damage - this.defense} points de vie`)
-    return this.pv -= damage - this.defense;
-  }
-}
+    set pm(newPm: number) {
+      this._pm = newPm;
+    }
+
+    get critic(): number {
+      return this._critic;
+    }
+
+    set critic(newCritic: number) {
+      this._critic = newCritic;
+    }
+
+//     public attack(): number {
+//       let damage: number;
+//       let coupCritique = Math.random();
+//       if (coupCritique < 0.1) {
+//         damage = this.force *2;
+//         console.log(`${this.nom} fait un coup critique et inflige ${damage} de dégats`)
+//       } else {
+//         damage = this.force
+//         console.log(`${this.nom} inflige ${damage} de dégats`)
+//       }
+//       if (this.arme.possedeVolDeVie() && this.pv < this.pvMax){
+//         let stealedPv = Math.trunc((this.force) * 0.13);
+//         if (this.pv + stealedPv > this.pvMax) {
+//           console.log(`${this.nom} récupère tous ses points de vie`)
+//           this.pv += this.pvMax
+//         } else {
+//           this.pv += this.pv + stealedPv
+//           console.log(`${this.nom} récupère ${stealedPv} points de vie`)
+//         }
+//       } else if (this.arme.possedeVolDeVie() && this.pv === this.pvMax) {
+//         console.log(`${this.nom} ne récupère pas de vie`)
+//       }
+//     return damage;
+//   }
+
+//   public takeDamage(damage: number) {
+//     console.log(`${this.nom} perd ${damage - this.defense} points de vie`)
+//     return this.pv -= damage - this.defense;
+//   }
+// }
