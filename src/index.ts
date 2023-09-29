fdc564845e2a2c47ab1c4f557c22eadb1ce8f8f7
Zelie-C <zelie.chemin@gmail.com>
2023-09-29 16:30:09 +0200

    potion et equipement utilisables

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import { PlayerTeam } from "./Team/PlayerTeam";
 import { Viking } from "./CharaterType/Viking";
 import { Wizard } from "./CharaterType/Wizard";
 import { NpcTeam } from "./Team/NpcTeam";
+import { Potion } from "./Objet/Potion";
+import { Axe } from "./Objet/Axe";
 
-const Ragnar = new Character("Ragnar", new Viking, new PlayerTeam)
+const lifePotion = new Potion("Potion de vie", 15, 0.5, 1, 100)
+const axe = new Axe()
+const Ragnar = new Character("Ragnar", new Viking, new PlayerTeam, [axe], [lifePotion])
 
-const Lizard = new Character("Lizard", new Wizard, new NpcTeam)
-console.log(Lizard)
+Ragnar.consommable![0].use(Ragnar)
+
+// const Lizard = new Character("Lizard", new Wizard, new NpcTeam)
+// console.log(Lizard)
 
 // const lameDeSang = new Arme("Lame de Sang", 4);
 // const hache = new Arme("Hache", 8)
